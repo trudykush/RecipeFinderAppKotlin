414e19e8e60d4ba8b412d75987fbdd18d0fc3a64
Kush <kushal@campion.ie>
2020-05-18 21:08:24 +0100

    Final Application for food finder

---
+++ RecipeFinder/app/src/main/java/com/kush/recipefinder/activities/RecipeList.kt
@@ -0,0 +1,12 @@
+package com.kush.recipefinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipeList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_list)
+    }
+}
---
+++ RecipeFinder/app/src/main/java/com/kush/recipefinder/model/Recipe.kt
@@ -0,0 +1,4 @@
+package com.kush.recipefinder.model
+
+class Recipe {
+}
